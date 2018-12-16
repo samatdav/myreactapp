2a159389d8f1bfd3c82b547576e8b7bcf47fbc64
samatdav <samatdav@gmail.com>
2018-12-17 05:20:26 +0900

    Updates

---
+++ precache-manifest.f43401de2230f3790deeb35bf3799828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "220c3d512945c82600be",
+    "url": "/myreactapp/static/css/main.fa3419fc.chunk.css"
+  },
+  {
+    "revision": "220c3d512945c82600be",
+    "url": "/myreactapp/static/js/main.220c3d51.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/myreactapp/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "0ce04c2388bba3e2f0cf",
+    "url": "/myreactapp/static/js/runtime~main.0ce04c23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7e32b533cdba66dda2a28ccf43df5538",
+    "url": "/myreactapp/index.html"
+  }
+];
